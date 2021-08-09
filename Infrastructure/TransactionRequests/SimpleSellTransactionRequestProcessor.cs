9c30de8bcd49c159e3bf42460fa972a5dacbafeb
Ales Tepina <ales.tepina@studio-moderna.com>
2021-08-09 14:45:49 +0200

    Added two more tests to Sell transaction processor

--- Infrastructure/TransactionRequests/SimpleSellTransactionRequestProcessor.cs
+++ Infrastructure/TransactionRequests/SimpleSellTransactionRequestProcessor.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using Core.Implementations.DTOs;
 using Core.Interfaces;
 
@@ -13,7 +14,26 @@ namespace Infrastructure.TransactionRequests
             {
                 if (cryptoExchanges.Any())
                 {
-                    
+                    var exchangesWithNonEmptyBidList = (from ce in cryptoExchanges
+                        where ce.OrderBook.Bids.Count > 0
+                        select ce).ToList();
+
+                    if (exchangesWithNonEmptyBidList.Count > 0)
+                    {
+                        var dummyExchange = exchangesWithNonEmptyBidList[0];
+
+                        if (dummyExchange.OrderBook.Bids[0].Order.Amount >= transactionRequest.TransactionAmount)
+                        {
+                            return new RequestProcessorResult()
+                                { TransactionIsValid = true };
+                        }
+
+                        return new RequestProcessorResult()
+                            { TransactionIsValid = false, ErrorMessage = "No crypto exchanges with bid high enough to satisfy the request exist." };
+                    }
+
+                    return new RequestProcessorResult()
+                        { TransactionIsValid = false, ErrorMessage = "No crypto exchanges with non-empty bid list exist." };
                 }
                 else
                 {
--- UnitTests/SellTransactionRequestProcessorTests.cs
+++ UnitTests/SellTransactionRequestProcessorTests.cs
@@ -34,6 +34,43 @@ namespace UnitTests
             Assert.AreEqual("No crypto exchanges exist.", result.ErrorMessage);
         }
 
+        [Test]
+        public void T03_ProcessTransaction_OneExchangeExistsButHasNoBids_TransactionIsInvalid()
+        {
+            var sellTransactionRequestProcessor = CreateSellTransactionRequestProcessor();
+            var stubTransactionRequest = new TransactionRequest { TransactionAmount = 1m, OrderType = OrderType.Sell };
+
+            var stubCryptoExchanges = new List<CryptoExchange>()
+            {
+                new CryptoExchange() {Name = "StubCryptoExchange", OrderBook = new OrderBook() {Bids = new List<Bid>()}}
+            };
+            var result = sellTransactionRequestProcessor.ProcessTransaction(stubTransactionRequest, stubCryptoExchanges);
+
+            Assert.IsFalse(result.TransactionIsValid);
+            Assert.AreEqual("No crypto exchanges with non-empty bid list exist.", result.ErrorMessage);
+        }
+
+        [Test]
+        public void T04_ProcessTransaction_OneExchangeExistsWithOneBidThatIsLowerThanRequestedTransaction_TransactionIsInvalid()
+        {
+            var sellTransactionRequestProcessor = CreateSellTransactionRequestProcessor();
+            var stubTransactionRequest = new TransactionRequest { TransactionAmount = 1m, OrderType = OrderType.Sell };
+
+            var stubBid = new Bid {Order = new Order() {Amount = 0.5m}};
+            var stubCryptoExchanges = new List<CryptoExchange>()
+            {
+                new CryptoExchange() {Name = "StubCryptoExchange", OrderBook = new OrderBook() {Bids = new List<Bid>()
+                    {
+                        stubBid
+                    }}}
+            };
+            var result = sellTransactionRequestProcessor.ProcessTransaction(stubTransactionRequest, stubCryptoExchanges);
+
+            Assert.IsFalse(result.TransactionIsValid);
+            Assert.AreEqual("No crypto exchanges with bid high enough to satisfy the request exist.",
+                result.ErrorMessage);
+        }
+
         #region HelperMethods
         private static ISellTransactionRequestProcessor CreateSellTransactionRequestProcessor()
         {
