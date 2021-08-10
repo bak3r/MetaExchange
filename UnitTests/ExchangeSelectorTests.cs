d21b6f7754477e347256f19d051b9e758c5edb9d
Ales Tepina <ales.tepina@studio-moderna.com>
2021-08-10 15:42:17 +0200

    Added exchange selector test for bids

--- UnitTests/ExchangeSelectorTests.cs
+++ UnitTests/ExchangeSelectorTests.cs
@@ -13,8 +13,8 @@ namespace UnitTests
         {
             var simpleExchangeLocator = new SimpleExchangeSelector();
 
-            var stubMultipleExchangesWithAsks = new Dictionary<string, List<Ask>>();
-            var result = simpleExchangeLocator.FindExchangeWithLowestPossibleAskTransactionCost(stubMultipleExchangesWithAsks);
+            var stubEmptyAskDictionary = new Dictionary<string, List<Ask>>();
+            var result = simpleExchangeLocator.FindExchangeWithLowestPossibleAskTransactionCost(stubEmptyAskDictionary);
 
             Assert.IsNull(result.Item1);
             Assert.IsNull(result.Item2);
@@ -40,24 +40,22 @@ namespace UnitTests
 
             var stubMultipleExchangesWithAsks = new Dictionary<string, List<Ask>>();
 
-            var exchangeName1 = "StubExchange1";
-            var exchange1askList = new List<Ask>();
-            var ask1 = new Ask();
-            ask1.Order = new Order() { Amount = 1m, Price = 1m };
-            exchange1askList.Add(ask1);
+            const string exchangeName1 = "StubExchange1";
+            var exchange1AskList = new List<Ask>();
+            var ask1 = new Ask {Order = new Order() {Amount = 1m, Price = 1m}};
+            exchange1AskList.Add(ask1);
 
-            var exchangeName2 = "StubExchange2";
-            var exchange2askList = new List<Ask>();
-            var ask2 = new Ask();
-            ask2.Order = new Order() { Amount = 1m, Price = 2m };
-            exchange2askList.Add(ask2);
+            const string exchangeName2 = "StubExchange2";
+            var exchange2AskList = new List<Ask>();
+            var ask2 = new Ask {Order = new Order() {Amount = 1m, Price = 2m}};
+            exchange2AskList.Add(ask2);
 
-            stubMultipleExchangesWithAsks.Add(exchangeName1, exchange1askList);
-            stubMultipleExchangesWithAsks.Add(exchangeName2, exchange2askList);
+            stubMultipleExchangesWithAsks.Add(exchangeName1, exchange1AskList);
+            stubMultipleExchangesWithAsks.Add(exchangeName2, exchange2AskList);
 
             var result = simpleExchangeLocator.FindExchangeWithLowestPossibleAskTransactionCost(stubMultipleExchangesWithAsks);
 
-            Assert.AreEqual(result.Item1, exchangeName1);
+            Assert.AreEqual(exchangeName1, result.Item1);
             Assert.Contains(ask1, result.Item2);
         }
 
@@ -68,26 +66,106 @@ namespace UnitTests
 
             var stubMultipleExchangesWithAsks = new Dictionary<string, List<Ask>>();
 
-            var exchangeName1 = "StubExchange1";
-            var exchange1askList = new List<Ask>();
-            var ask1 = new Ask();
-            ask1.Order = new Order() { Amount = 1m, Price = 1m, Id = "1" };
-            exchange1askList.Add(ask1);
+            const string exchangeName1 = "StubExchange1";
+            var exchange1AskList = new List<Ask>();
+            var ask1 = new Ask {Order = new Order() {Amount = 1m, Price = 1m, Id = "1"}};
+            exchange1AskList.Add(ask1);
 
-            var exchangeName2 = "StubExchange2";
-            var exchange2askList = new List<Ask>();
-            var ask2 = new Ask();
-            ask2.Order = new Order() { Amount = 1m, Price = 1m, Id = "2" };
-            exchange2askList.Add(ask2);
+            const string exchangeName2 = "StubExchange2";
+            var exchange2AskList = new List<Ask>();
+            var ask2 = new Ask {Order = new Order() {Amount = 1m, Price = 1m, Id = "2"}};
+            exchange2AskList.Add(ask2);
 
-            stubMultipleExchangesWithAsks.Add(exchangeName1, exchange1askList);
-            stubMultipleExchangesWithAsks.Add(exchangeName2, exchange2askList);
+            stubMultipleExchangesWithAsks.Add(exchangeName1, exchange1AskList);
+            stubMultipleExchangesWithAsks.Add(exchangeName2, exchange2AskList);
 
             var result = simpleExchangeLocator.FindExchangeWithLowestPossibleAskTransactionCost(stubMultipleExchangesWithAsks);
 
             Assert.NotNull(result.Item1);
             Assert.NotNull(result.Item2);
             Assert.True(result.Item2.Count == 1);
+        }
+
+        [Test]
+        public void T05_FindExchangeWithHighestPossibleBidTransactionValue_NoExchangesExist_NullIsReturned()
+        {
+            var simpleExchangeLocator = new SimpleExchangeSelector();
+
+            var stubEmptyExchangeDictionary = new Dictionary<string, List<Bid>>();
+            var result = simpleExchangeLocator.FindExchangeWithHighestPossibleBidTransactionValue(stubEmptyExchangeDictionary);
+
+            Assert.IsNull(result.Item1);
+            Assert.IsNull(result.Item2);
+        }
+
+        [Test]
+        public void T06_FindExchangeWithHighestPossibleBidTransactionValue_OnlyExchangeWithEmptyBidListExist_NullIsReturned()
+        {
+            var simpleExchangeLocator = new SimpleExchangeSelector();
+
+            var stubEmptyExchangeDictionary = new Dictionary<string, List<Bid>>();
+            stubEmptyExchangeDictionary.Add("StubExchangeName", new List<Bid>());
+            var result = simpleExchangeLocator.FindExchangeWithHighestPossibleBidTransactionValue(stubEmptyExchangeDictionary);
+
+            Assert.IsNull(result.Item1);
+            Assert.IsNull(result.Item2);
+        }
+
+        [Test]
+        public void T07_FindExchangeWithHighestPossibleBidTransactionValue_MultipleExchangesDifferentBidSumsExist_ExchangeWithHighestBidSumIsReturned()
+        {
+            var simpleExchangeLocator = new SimpleExchangeSelector();
+
+            var stubExchange1Bid1 = new Bid {Order = new Order() {Amount = 1m, Price = 1m}};
+            var stubExchange1Bid2 = new Bid {Order = new Order() {Amount = 2m, Price = 2m}};
+            var stubCryptoExchange1 = new CryptoExchange()
+                {Name = "StubExchange1", OrderBook = new OrderBook() {Bids = {stubExchange1Bid1, stubExchange1Bid2}}};
+
+            var stubExchange2Bid1 = new Bid {Order = new Order() {Amount = 1m, Price = 3m}};
+            var stubBid2 = new Bid() {Order = new Order() {Amount = 2m, Price = 4m}};
+            var stubExchange2Bid2 = stubBid2;
+            var stubCryptoExchange2 = new CryptoExchange()
+                {Name = "StubExchange2", OrderBook = new OrderBook() {Bids = {stubExchange2Bid1, stubExchange2Bid2}}};
+
+            var stubExchangeDictionary = new Dictionary<string, List<Bid>>
+            {
+                {stubCryptoExchange1.Name, stubCryptoExchange1.OrderBook.Bids},
+                {stubCryptoExchange2.Name, stubCryptoExchange2.OrderBook.Bids}
+            };
+
+            var result = simpleExchangeLocator.FindExchangeWithHighestPossibleBidTransactionValue(stubExchangeDictionary);
+
+            Assert.AreEqual(stubCryptoExchange2.Name, result.Item1);
+            Assert.Contains(stubBid2, result.Item2);
+        }
+
+        [Test]
+        public void T08_FindExchangeWithHighestPossibleBidTransactionValue_MultipleExchangesWithSameBidSumsExist_OnlyOneExchangeIsReturned()
+        {
+            var simpleExchangeLocator = new SimpleExchangeSelector();
+
+            var stubExchange1Bid1 = new Bid { Order = new Order() { Amount = 1m, Price = 2m } };
+            var stubCryptoExchange1 = new CryptoExchange()
+                { Name = "StubExchange1", OrderBook = new OrderBook() { Bids = { stubExchange1Bid1 } } };
+
+            var stubExchange2Bid1 = new Bid { Order = new Order() { Amount = 1m, Price = 2m } };
+            
+            var stubCryptoExchange2 = new CryptoExchange()
+                { Name = "StubExchange2", OrderBook = new OrderBook() { Bids = { stubExchange2Bid1 } } };
+
+            var stubExchangeDictionary = new Dictionary<string, List<Bid>>
+            {
+                {stubCryptoExchange1.Name, stubCryptoExchange1.OrderBook.Bids},
+                {stubCryptoExchange2.Name, stubCryptoExchange2.OrderBook.Bids}
+            };
+
+            var result = simpleExchangeLocator.FindExchangeWithHighestPossibleBidTransactionValue(stubExchangeDictionary);
+
+            Assert.NotNull(result.Item1);
+            Assert.NotNull(result.Item2);
+            Assert.True(result.Item2.Count == 1);
+            Assert.AreEqual(1, result.Item2[0].Order.Amount);
+            Assert.AreEqual(2, result.Item2[0].Order.Price);
         }
     }
 }
