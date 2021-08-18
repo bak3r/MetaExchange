22ac8dfd52d604f1895ec242b633fd41e1d0a3be
Ales Tepina <ales.tepina@studio-moderna.com>
2021-08-18 14:28:08 +0200

    Added comparisson logic for Asks that have equal price

--- Core/Implementations/DTOs/Ask.cs
+++ Core/Implementations/DTOs/Ask.cs
@@ -10,6 +10,16 @@ namespace Core.Implementations.DTOs
             var otherAsk = (Ask)obj;
             if (this.Order.Price < otherAsk.Order.Price) return -1;
             if (this.Order.Price > otherAsk.Order.Price) return 1;
+            if (otherAsk.Order.Price == this.Order.Price)
+            {
+                // Prefer asks with largest amount so that number
+                // of sell transactions are kept to a minimum because
+                // of transaction fees
+                var otherAskMultiplication = otherAsk.Order.Price * otherAsk.Order.Amount;
+                var thisAskMultiplication = this.Order.Price * this.Order.Amount;
+                if (otherAskMultiplication < thisAskMultiplication) return 1;
+                if (otherAskMultiplication > thisAskMultiplication) return -1;
+            }
             return 0;
         }
     }
